d2d54125979ea5fe65a768aab9192c26b4bfa652
LuminRabbit <144849856+LuminRabbit@users.noreply.github.com>
2026-02-01 10:10:51 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,43 @@
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("poker-hands").then(cache =>
-      cache.addAll(["./", "./index.html", "./app.js"])
-    )
+const CACHE_NAME = "poker-hands-v2";
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./app.js",
+  "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, clone);
+        });
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
